9dad76560cda767bbf6c8494e6499d71c1a19170
Mikhail Knyazev <knyazevmo@gmail.com>
2020-09-15 16:05:55 +0300

    комменты

--- server.py
+++ server.py
@@ -3,13 +3,14 @@ from weather import weather_by_city
 
 app = Flask(__name__)
 
+#используем декоратор
 @app.route("/")
 def index():
     weather = weather_by_city('Moscow,Russia')
     if weather:
-        return f"Сейчас на улице {weather['temp_C']}, но ощущается как {weather['FeelsLikeC']}"
+        return f"Сейчас на улице {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
     else:
         return "Погода недоступна"
-
+#запускаем приложение используя дебаг=Тру(можно оставить пустые скобки и ничего не использовать)
 if __name__ == "__main__":
     app.run(debug=True)
