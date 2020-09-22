41dc4a11d167541d4622baeac4034dea03543eae
Mikhail Knyazev <knyazevmo@gmail.com>
2020-09-22 17:34:05 +0300

    Прикрутили новости пайтон

--- server.py
+++ server.py
@@ -1,26 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
+from python_org_news import get_python_news
 
 app = Flask(__name__)
 
 #используем декоратор
 @app.route("/")
 def index():
-    weather = weather_by_city('Moscow,Russia')
-    if weather:
-        weather_text = f"Сейчас на улице {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
-    else:
-        weather_text = "Погода недоступна"
-    return f"""
-    <html>
-        <head>
-            <title>Прогноз погоды</title>
-        </head>
-        <body>
-            <h1>{weather_text}</h1>
-        </body>
-    </html>
-    """
+    page_title = "Новости python"
+    weather = weather_by_city('Petersburg,Russia')
+    news_list = get_python_news()
+    return render_template('index.html', page_title=page_title, weather=weather, news_list=news_list) # возвращаем шаблон
+
+
 #запускаем приложение используя дебаг=Тру(можно оставить пустые скобки и ничего не использовать)
 if __name__ == "__main__":
     app.run(debug=True)
--- weather.py
+++ weather.py
@@ -29,4 +29,4 @@ def weather_by_city(city_name):
     return False
 
 if __name__ == "__main__":
-    print(weather_by_city('Moscow,Russia'))
+    print(weather_by_city('Petersburg,Russia'))
